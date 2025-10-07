7e933eb2eac82e8c6a76dd225cbdd41814156dd3
Chu Thị Hồng <chuhoa914@gmail.com>
2025-10-07 20:31:12 +0700

    truoc khi merge

--- english_app/src/main/java/com/back_end/english_app/repository/UserRepository.java
+++ english_app/src/main/java/com/back_end/english_app/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.back_end.english_app.repository;
 
 import com.back_end.english_app.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity , Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findById(Long id);
 }
